e6aad4493d1151515008b3d47b3aeabd55068267
samjithin <samjithin@gmail.com>
2021-03-19 15:42:18 +0530

    d

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "instance_name" {
-    value       = google_compute_instance.vm_instance.name
-    description = "details of the vm"
-}
+#output "instance_name" {
+#    value       = google_compute_instance.vm_instance.name
+#    description = "details of the vm"
+#}
