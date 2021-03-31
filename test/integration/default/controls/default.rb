92dc3edf20b86c5b96ff932469f6d022bbef86bc
samjithin <samjithin@gmail.com>
2021-03-31 21:46:42 +0530

    Modified workflow

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -5,7 +5,7 @@ instance_name = attribute('instance_name')
 control "default" do
  title 'Ensure GCE instance_name is running'
 
-  describe google_compute_instance(project: 'terraformlearning-284414',  zone: 'us-west1-b', name: test1) do
+  describe google_compute_instance(project: 'terraformlearning-284414',  zone: 'us-west1-b', name: 'test1') do
     its('status') { should eq 'RUNNING' }
     its('machine_type') { should match "f1-micro" }
     its('tags.items') { should include 'web' }
