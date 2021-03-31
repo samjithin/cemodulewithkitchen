2fb1673123849cbeb88a88dbdca5aa3ac3c04495
samjithin <samjithin@gmail.com>
2021-03-31 23:25:01 +0530

    kitchen.mod

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -8,7 +8,7 @@ control "default" do
   describe google_compute_instance(project: 'terraformlearning-284414',  zone: 'us-west1-b', name: 'test1') do
     its('status') { should eq 'RUNNING' }
     its('machine_type') { should match "f1-micro" }
-    its('tags.items') { should include 'web1' }
+    its('tags.items') { should include 'web' }
     its('tags.items') { should include 'dev' }
     its('tag_count'){should eq 2}
   end
